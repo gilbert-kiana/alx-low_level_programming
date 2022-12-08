9270cb4497f5c8885f324d82be006f2fb211bb04
gilbert-kiana <gilbertintabo@gmail.com>
2022-12-08 13:05:14 -0800

    t

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,10 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	printf("Size of  a char: %1u byte(s)\n", sizeof(char));
+	printf("Size of  a int: %1u byte(s)\n", sizeof(int));
+	printf("Size of  a char: %1u byte(s)\n", sizeof(long int));
+	printf("Size of  a char: %1u byte(s)\n", sizeof(long long int));
+	printf("Size of  a char: %1u byte(s)\n", sizeof(float));
+	return (0);
 }
