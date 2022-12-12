0e9110ef4354d171d2ab573e5ea2c61ab11245ae
gilbert-kiana <gilbertintabo@gmail.com>
2022-12-12 01:58:06 -0800

    add

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main -Entry
- * Return: 1
- */
+ *  *  main - Enty point
+ *   *
+ *    *  Return: Always 0 (Success)
+ *     */
+
 int main(void)
 {
-	printf("Size of  a char: %1u byte(s)\n", sizeof(char));
-	printf("Size of  a int: %1u byte(s)\n", sizeof(int));
-	printf("Size of  a char: %1u byte(s)\n", sizeof(long int));
-	printf("Size of  a char: %1u byte(s)\n", sizeof(long long int));
-	printf("Size of  a char: %1u byte(s)\n", sizeof(float));
-	return (0);
+		char acharacter;
+			int ainteger;
+				long along;
+					long long alonglong;
+						float afloat;
+
+							printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+								printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+									printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+										printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+											printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+												return (0);
+
 }
