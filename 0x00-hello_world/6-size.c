2ddf8a838ba54c5b2c7b9835b26f76d8b0c1d967
gilbert-kiana <gilbertintabo@gmail.com>
2022-12-12 02:01:01 -0800

    add

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  *  main - Enty point
- *   *
- *    *  Return: Always 0 (Success)
- *     */
+ *     main - Enty point
+ *  
+ *     Return: Always 0 (Success)
+ * */
 
 int main(void)
 {
