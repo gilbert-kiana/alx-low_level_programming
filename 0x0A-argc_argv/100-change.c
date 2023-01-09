776e82914eb5824a9ff66497f4752fea6b14b5c0
gilbert-kiana <gilbertintabo@gmail.com>
2023-01-09 07:03:06 -0800

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,4 @@
-
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -25,15 +24,18 @@ int main(int args, char *argv[])
 		return (0);
 	}
 	for (; c >= 0;)
+
 	{
 		if (c >= 25)
 			c -= 25;
+
 
 		else if (c >= 10)
 			c -= 10;
 
 		else if (c >= 5)
 			c -= 5;
+
 
 		else if (c >= 2)
 			c -= 2;
